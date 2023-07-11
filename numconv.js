7ee6352e8ed7b56bd040a927c937a14da7395d8b
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-07-11 16:13:42 +0700

    create

---
+++ numconv.js
@@ -0,0 +1,26 @@
+let num1 = "150";
+let flo1 = "1.50";
+
+console.log("*******Concerting string to integers*******");
+
+console.log(parseInt("100"));
+console.log(parseInt(num1));
+console.log(parseInt("ABC"));
+console.log(parseInt("0xF"));
+
+console.log("******Converting string to float*******");
+
+console.log(parseFloat("1.25abc55"));
+console.log(parseFloat(flo1));
+console.log(parseFloat("ABC"));
+
+console.log("*******More Conversion Examples*******");
+
+console.log(parseInt("1.5"));
+console.log(parseInt("1+1"));
+
+console.log("*******Converting numbers to string*******");
+
+console.log(num1.toString());
+console.log(flo1.toString());
+console.log((100).toString());
